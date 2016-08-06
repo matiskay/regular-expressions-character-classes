c9e94baa547bbe12aff35249c90c7c202f5c5804
Edgar Marca <matiskay@gmail.com>
2016-08-06 00:47:02 -0500

    Solve name regex exercise.

--- lib/character_classes.rb
+++ lib/character_classes.rb
@@ -3,7 +3,7 @@ class CharacterClasses
   # Returns a regex that matches a string that has at least three characters, like
   # "abc" or "a12" or "a b" (the middle character is a space).
   def matching_at_least_three_characters
-    /WRITE ME/
+    /[a-zA-Z0-9 ]{3,}/
   end
 
   # Returns a regex that matches a telephone number with dashes.
@@ -16,14 +16,14 @@ class CharacterClasses
   #   * "650 123 1234"
   #   * "650 123 12"
   def matching_telephone_number
-    /WRITE ME/
+    /\d{3}-\d{3}-\d{3}/
   end
 
   # Returns a regex that matches any string with two single-digit numbers in it.
   #
   # Examples: "23", "a 2 b 4".
   def matching_two_numbers
-    /WRITE ME/
+    /\d\D*\d/
   end
 
   # Returns a regex that matches a string that has at least one character that
@@ -38,6 +38,6 @@ class CharacterClasses
   #   * "james"
   #   * "abc"
   def matching_not_lowercase
-    /WRITE ME/
+    /([A-Z]|\d)/
   end
 end
